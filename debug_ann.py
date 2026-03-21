ec5c5319d6e36ea274c9020344db8ae9c6a8b16a
Satpal26 <satpalgandhi8935@gmail.com>
2026-03-21 15:43:19 +0530

    docs: add paper v6 with ML pipeline results and 120 tests

---
+++ debug_ann.py
@@ -0,0 +1,8 @@
+﻿from neurobids_flow.moabb_wrapper import NBIDSFDataset
+
+dataset = NBIDSFDataset(bids_root='./bids_output', task='workload')
+data = dataset._get_single_subject_data(1)
+raw = data['session_01']['run_0']
+print('Annotations:')
+for ann in raw.annotations:
+    print('  desc=' + ann['description'] + '  onset=' + str(round(ann['onset'], 2)))
---
+++ test_wrapper_quick.py
@@ -0,0 +1,11 @@
+﻿from src.neurobids_flow.moabb_wrapper import NBIDSFDataset
+
+dataset = NBIDSFDataset(bids_root='./bids_output', task='workload')
+print('Subjects:', dataset.subject_list)
+print('Sessions:', dataset._sessions)
+print('Events:', list(dataset.event_id.keys()))
+
+data = dataset._get_single_subject_data(1)
+for sess, runs in data.items():
+    for run, raw in runs.items():
+        print(f'{sess}/{run} | ch={len(raw.ch_names)} | dur={raw.times[-1]:.1f}s | annotations={len(raw.annotations)}')
